82658e72d6db828571ffdac7588a6563fb88d784
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-03-09 09:28:25 +0800

    first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ src/esunconsulting/core.clj
@@ -0,0 +1,6 @@
+(ns esunconsulting.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/esunconsulting/core_test.clj
@@ -0,0 +1,7 @@
+(ns esunconsulting.core-test
+  (:require [clojure.test :refer :all]
+            [esunconsulting.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
